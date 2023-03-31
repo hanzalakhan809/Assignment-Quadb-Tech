6f3b62944df5dc6042b1f21355c70025295ba2c1
MOHAMMAD HANZALA KHAN <31920090+hanzalakhan809@users.noreply.github.com>
2023-04-01 01:35:11 +0530

    completed final

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 function About() {
   return (
-    <>
-      <h5 className='text-center'>To be made</h5>
-    </>
+    <div className="card my-2 mx-auto " >
+      <div className="card-body">
+        <h5 className="card-text">
+
+          Grab your favourite seats & avail cashback & offers. Book Now on BookMyShow. Check out the showtimes of the latest releases in cinemas near you. Safe Cinema Experience. 24/7 Customer Support. Book Tickets Online. Find Showtimes Near You and offers showtimes, movie tickets, reviews, trailers, concert tickets and events nearby. Also features promotional offers, coupons and mobile app..</h5>
+
+      </div>
+    </div>
   )
 }
 
-export default About``
+export default About
--- src/components/BookTicket.js
+++ src/components/BookTicket.js
@@ -5,20 +5,21 @@ import { useNavigate, useLocation } from 'react-router-dom';
 export default function BookTicket() {
   const location = useLocation();
   let nameOfMovie = location.state.data.name;
-
   const navigate = useNavigate();
-  const [credentials, setcredentials] = useState({ email: '', date: '', time: '' })
+  const [credentials, setcredentials] = useState({ customerName: '', cardNumber: '', date: '', cvv: '' })
   const bookingInfo = {
-    email: credentials.email,
+    customerName: credentials.customerName,
+    cardNumber: credentials.cardNumber,
     date: credentials.date,
-    time: credentials.time
+    cvv: credentials.cvv
   }
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     localStorage.setItem('bookingInfo', JSON.stringify(bookingInfo));
     navigate('/home')
-    alert(`Ticked booked Successfully for Date-${bookingInfo.date},Time-${bookingInfo.time}`)
+    alert(`Ticked booked Successfully for Date-${bookingInfo.date}`)
+
   }
 
 
@@ -30,35 +31,38 @@ export default function BookTicket() {
 
 
   return (
+    <>
+      <div className="container">
+        <form className='mx-4' onSubmit={handleSubmit} >
+          <div className="container mt-3">
+            <h2>Payment Options</h2>
+            <h2 className='text-center'>{`Movie Name-${nameOfMovie}`}</h2>
+          </div>
+          <div className="mb-3" >
+            <label htmlFor="exampleInputEmail1" className="form-label" >Full Name</label>
+            <input type="text" className="form-control" name='customerName' id="exampleInputEmail1" aria-describedby="emailHelp" onChange={onChange
+            } required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="card-number" className="form-label">Card Number</label>
+            <input className="form-control" id="card-number" onChange={onChange
+            } name='cardNumber' required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="cvv" className="form-label">CVV</label>
+            <input type="password" autoComplete='flase' maxLength="4" className="form-control" id="cvv" onChange={onChange
+            } name='cvv' required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputDate1" className="form-label">Date</label>
+            <input type="date" className="form-control" id="exampleInputDate1" onChange={onChange
+            } name='date' autoComplete='current-date' required />
+          </div>
 
-    <div className="container">
-      <form className='mx-4' onSubmit={handleSubmit} >
-        <div className="container mt-3">
-          <h2>Book Your Ticket....</h2>
-          <h1 className='text-center'>{`Movie Name-${nameOfMovie}`}</h1>
-        </div>
-        <div className="mb-3" >
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" autoComplete='current-email' name='email' id="exampleInputEmail1" aria-describedby="emailHelp" onChange={onChange
-          } />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputDate1" className="form-label">Date</label>
-          <input type="date" className="form-control" id="exampleInputDate1" onChange={onChange
-          } name='date' autoComplete='current-date' />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputTime1" className="form-label">Time</label>
-          <input type="time" className="form-control" id="exampleInputTime1" onChange={onChange
-          } name='time' autoComplete='current-time' />
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1">I am Not a Robot</label>
-        </div>
-        <button type="submit" className="btn btn-primary" >Submit</button>
-      </form>
-    </div>
+          <button type="submit" className="btn btn-primary" >Submit</button>
+        </form>
+      </div>
+    </>
+
   )
 }
